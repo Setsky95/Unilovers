3c407f3bb795eb6b1df3046d5aafa653daa22511
Sebastian Pavlotsky <sebapavlotsky@gmail.com>
2025-05-27 20:26:04 -0300

    routes and gift

--- src/components/common/Gift&carrousel/Gift/PromoResult.jsx
+++ src/components/common/Gift&carrousel/Gift/PromoResult.jsx
@@ -1,12 +1,18 @@
 import "./Gift.css";
 
-const PromoResult = ({ onClick,promotionTitle, promotionBenefit }) => {
+const PromoResult = ({ onClick, promotionTitle, promotionBenefit }) => {
   return (
     <div className="promo-result">
-      <h2 className="promo-benefit"> {promotionTitle}</h2>
-      <h2 className="promo-benefit"> {promotionBenefit}</h2>
-      <div className="quitPromoButtonContainer">
-        <button className="quitPromoButton" onClick={onClick} >x</button>
+      <div className="glass-effect"></div>
+      <div className="button-text">
+        <h2 className="promo-benefit promo-text">{promotionTitle}</h2>
+        <h2 className="promo-benefit promo-text">{promotionBenefit}</h2>
+        <div className="quitPromoButtonContainer">
+          <button className="cart-button" onClick={onClick}>
+            <div className="glass-effect"></div>
+            <span className="button-text">x</span>
+          </button>
+        </div>
       </div>
     </div>
   );
